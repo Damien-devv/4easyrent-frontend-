fab6f685ef530c47e91d367fd1b1183e5cc8c373
Damien <beaunomdamien@gmail.com>
2025-03-18 21:45:38 +0400

    Add Next.js app boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.html',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@css': path.resolve(__dirname, 'css'),
+      '@assets': path.resolve(__dirname, 'assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, '.'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
   },
   module: {
     rules: [
@@ -16,13 +26,24 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
   devServer: {
     static: {
-      directory: path.join(__dirname, '.'),
+      directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
+    hot: true,
   },
 };
